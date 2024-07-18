2fd6c4b61de4c97067ff19b24d070f36138590a6
levantung <levantung0000@gmail.com>
2024-07-18 15:30:18 +0700

    a

--- NoMango/NoMango/obj/Debug/net7.0/NoMango.AssemblyInfo.cs
+++ NoMango/NoMango/obj/Debug/net7.0/NoMango.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NoMango")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bed743539897ea521a9a34c052692c9a458feba")]
 [assembly: System.Reflection.AssemblyProductAttribute("NoMango")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NoMango")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
