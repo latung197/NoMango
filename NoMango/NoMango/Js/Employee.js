396031bd0b0a0e566a7dbb57c7cf29aed6ee5c55
levantung <levantung0000@gmail.com>
2024-08-16 16:56:48 +0700

    a

--- NoMango/NoMango/Js/Employee.js
+++ NoMango/NoMango/Js/Employee.js
@@ -5,14 +5,14 @@ loadData(1);
 function loadData(msg) {
     $.each(data, function (index, item) {
         var trHTML = $(`<tr>
-        <td>`+ item.EmployeeCode + `</td>
-        <td>`+ item.EmpooyeeName + `</td>
-        <td>`+ item.Mobile + `</td>  
-        <td>`+ item.Email + `</td>
-        <td>`+ item.Address + `</td>
-        <td>`+ item.cty + `</td>
+            <td>`+ item.EmployeeCode + `</td>
+            <td>`+ item.EmpooyeeName + `</td>
+            <td>`+ item.Mobile + `</td>  
+            <td>`+ item.Email + `</td>
+            <td>`+ item.Address + `</td>
+            <td>`+ item.cty + `</td>
         
-    </tr>`);
+        </tr>`);
         $('.grid tbody').append(trHTML);
     });
 
@@ -27,7 +27,7 @@ var data = [
         Email: "Tung:V1997@",
         Mobile: "0978366106",
         Address: "Ha Noi",
-        cty:"Robotcom FA"
+        cty: "Robotcom FA"
     },
     {
         EmployeeCode: "Nv0005",
@@ -55,12 +55,12 @@ class CustumerJS {
     loadData(msg) {
         $.each(data, function (index, item) {
             var trHTML = $(`<tr>
-        <td>`+ item.EmployeeCode + `</td>
-        <td>`+ item.EmpooyeeName + `</td>
-        <td>`+ item.Mobile + `</td>  
-        <td>`+ item.Email + `</td>
-        <td>`+ item.Address + `</td>
-    </tr>`);
+            <td>`+ item.EmployeeCode + `</td>
+            <td>`+ item.EmpooyeeName + `</td>
+            <td>`+ item.Mobile + `</td>  
+            <td>`+ item.Email + `</td>
+            <td>`+ item.Address + `</td>
+        </tr>`);
             $('.grid tbody').append(trHTML);
         });
 
@@ -72,14 +72,59 @@ class CustumerJS {
         $('#btncancel').click(this.btncancel);
         $('#btncancel').click(this.btncancel);
         $('.dialog-button-close').click(this.btncancel);
+        $('#btnSave').click(this.btnSave);
+        $('#txtMa_nv').blur(this.checkReQuired)
     }
 
+    checkReQuired() {
+        $('.requied').addClass('requirederror');
+    }
+
+    btnSave() {
+        var employeCode = $('#txtMa_nv').val();
+        var employeName = $('#txtTen_nv').val();
+
+        if (!employeCode) {
+            //$('#txtMa_nv').addClass('requirederror');
+            $('#txtMa_nv').focus();
+            $('#txtMa_nv').attr("title", "Bạn cần nhập thông tin này!");
+            return;
+        }
+        else {
+            
+        }
+
+        if (!employeName) {
+            alert("Bạn phải nhập tên nhân viên!");
+            return;
+        }
+
+        var employee = {};
+        debugger;
+
+        employee.EmployeeCode = $('#txtMa_nv').val();
+        employee.EmpooyeeName = $('#txtTen_nv').val();
+        employee.Email = $('#txtEmail').val();
+        employee.Mobile = $('#txtMobile').val();
+        employee.cty = $('#txtCongTy').val();
+
+        data.push(employee);
+        this.this.loadData("xin chao");
+
+
+    }
+
+    
     btnAddOnClick() {
         $('.dialog-model').show();
+        $('.dialog').show();
+        $('#txtMa_nv').focus();
+
     }
 
     btncancel() {
         $('.dialog-model').hide();
+        $('.dialog').show();
     }
 
     edit() {
@@ -89,4 +134,7 @@ class CustumerJS {
     delete() {
 
     }
+
+
 }
+
