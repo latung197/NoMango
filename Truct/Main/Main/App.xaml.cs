4e94dca0ef209b1bc45a078b7380be1500f149b8
levantung <levantung0000@gmail.com>
2024-10-15 18:08:22 +0700

    a

---
+++ Truct/Main/Main/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using Main.Views;
+
+namespace Main
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected void ApplicationStart(object sender, StartupEventArgs e)
+        {
+            var loginView = new LoginView();
+            loginView.Show();
+        }
+    }
+
+}
