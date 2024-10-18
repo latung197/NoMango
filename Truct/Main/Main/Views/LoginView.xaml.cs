426b0c0642e52b96e8d9db9d2535b5c0afcaa889
levantung <levantung0000@gmail.com>
2024-10-18 17:38:34 +0700

    s

--- Truct/Main/Main/Views/LoginView.xaml.cs
+++ Truct/Main/Main/Views/LoginView.xaml.cs
@@ -23,5 +23,13 @@ namespace Main.Views
         {
             InitializeComponent();
         }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
     }
 }
