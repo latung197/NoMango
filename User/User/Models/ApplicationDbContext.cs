5d85059018b3d2d32c4748b14ec43f489eab9c6a
levantung <levantung0000@gmail.com>
2025-05-20 10:29:39 +0700

    confix project

--- User/User/Models/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace User.Models
-{
-    public class ApplicationDbContext: IdentityDbContext<User>
-    {
-    }
-}
--- User/User/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace User.Models
-{
-    public class User:IdentityUser
-    {
-        public string? Inttials {  get; set; }
-    }
-}
