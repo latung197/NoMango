01970977a054a26534b5becb2ed93be937ee2744
latung197 <levantung0000@gmail.com>
2025-10-15 22:35:48 +0700

    a

---
+++ Truct/PCWinForm/PCWinForm/UserControl/BaseFormControl.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace PCWinForm.UserControl
+{
+    partial class BaseFormControl
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        private void InitializeComponent()
+        {
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.pnlToolbar = new System.Windows.Forms.Panel();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnExportExcel = new System.Windows.Forms.Button();
+            this.btnImportExcel = new System.Windows.Forms.Button();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.pnlGrid = new System.Windows.Forms.Panel();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.pnlEdit = new System.Windows.Forms.Panel();
+            this.pnlEditContent = new System.Windows.Forms.Panel();
+            this.pnlEditButtons = new System.Windows.Forms.Panel();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pnlHeader.SuspendLayout();
+            this.pnlToolbar.SuspendLayout();
+            this.pnlGrid.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.pnlEdit.SuspendLayout();
+            this.pnlEditButtons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.BackColor = System.Drawing.Color.SteelBlue;
+            this.pnlHeader.Controls.Add(this.lblTitle);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(900, 35);
+            this.pnlHeader.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(12, 8);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(54, 21);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "TITLE";
+            // 
+            // pnlToolbar
+            // 
+            this.pnlToolbar.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pnlToolbar.Controls.Add(this.btnRefresh);
+            this.pnlToolbar.Controls.Add(this.btnExportExcel);
+            this.pnlToolbar.Controls.Add(this.btnImportExcel);
+            this.pnlToolbar.Controls.Add(this.btnPrint);
+            this.pnlToolbar.Controls.Add(this.btnCopy);
+            this.pnlToolbar.Controls.Add(this.btnDelete);
+            this.pnlToolbar.Controls.Add(this.btnEdit);
+            this.pnlToolbar.Controls.Add(this.btnAdd);
+            this.pnlToolbar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlToolbar.Location = new System.Drawing.Point(0, 35);
+            this.pnlToolbar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlToolbar.Name = "pnlToolbar";
+            this.pnlToolbar.Padding = new System.Windows.Forms.Padding(4);
+            this.pnlToolbar.Size = new System.Drawing.Size(900, 32);
+            this.pnlToolbar.TabIndex = 1;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            //this.btnRefresh.Image = global::ERP.Properties.Resources.refresh_icon;
+            this.btnRefresh.Location = new System.Drawing.Point(536, 4);
+            this.btnRefresh.Margin = new System.Windows.Forms.Padding(2);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(60, 24);
+            this.btnRefresh.TabIndex = 7;
+            this.btnRefresh.Text = "Làm mới";
+            this.btnRefresh.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnExportExcel
+            // 
+            this.btnExportExcel.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            //this.btnExportExcel.Image = global::ERP.Properties.Resources.export_icon;
+            this.btnExportExcel.Location = new System.Drawing.Point(458, 4);
+            this.btnExportExcel.Margin = new System.Windows.Forms.Padding(2);
+            this.btnExportExcel.Name = "btnExportExcel";
+            this.btnExportExcel.Size = new System.Drawing.Size(74, 24);
+            this.btnExportExcel.TabIndex = 6;
+            this.btnExportExcel.Text = "Xuất Excel";
+            this.btnExportExcel.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnExportExcel.UseVisualStyleBackColor = true;
+            this.btnExportExcel.Click += new System.EventHandler(this.btnExportExcel_Click);
+            // 
+            // btnImportExcel
+            // 
+            this.btnImportExcel.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            //this.btnImportExcel.Image = global::ERP.Properties.Resources.import_icon;
+            this.btnImportExcel.Location = new System.Drawing.Point(380, 4);
+            this.btnImportExcel.Margin = new System.Windows.Forms.Padding(2);
+            this.btnImportExcel.Name = "btnImportExcel";
+            this.btnImportExcel.Size = new System.Drawing.Size(74, 24);
+            this.btnImportExcel.TabIndex = 5;
+            this.btnImportExcel.Text = "Nhập Excel";
+            this.btnImportExcel.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnImportExcel.UseVisualStyleBackColor = true;
+            this.btnImportExcel.Click += new System.EventHandler(this.btnImportExcel_Click);
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            //this.btnPrint.Image = global::ERP.Properties.Resources.print_icon;
+            this.btnPrint.Location = new System.Drawing.Point(303, 4);
+            this.btnPrint.Margin = new System.Windows.Forms.Padding(2);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(45, 24);
+            this.btnPrint.TabIndex = 4;
+            this.btnPrint.Text = "In";
+            this.btnPrint.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            //this.btnCopy.Image = global::ERP.Properties.Resources.copy_icon;
+            this.btnCopy.Location = new System.Drawing.Point(226, 4);
+            this.btnCopy.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(60, 24);
+            this.btnCopy.TabIndex = 3;
+            this.btnCopy.Text = "Sao chép";
+            this.btnCopy.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            //this.btnDelete.Image = global::ERP.Properties.Resources.delete_icon;
+            this.btnDelete.Location = new System.Drawing.Point(148, 4);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(50, 24);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Xóa";
+            this.btnDelete.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            //this.btnEdit.Image = global::ERP.Properties.Resources.edit_icon;
+            this.btnEdit.Location = new System.Drawing.Point(71, 4);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(50, 24);
+            this.btnEdit.TabIndex = 1;
+            this.btnEdit.Text = "Sửa";
+            this.btnEdit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            //this.btnAdd.Image = global::ERP.Properties.Resources.add_icon;
+            this.btnAdd.Location = new System.Drawing.Point(8, 4);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(50, 24);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // pnlGrid
+            // 
+            this.pnlGrid.Controls.Add(this.dataGridView);
+            this.pnlGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlGrid.Location = new System.Drawing.Point(0, 67);
+            this.pnlGrid.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlGrid.Name = "pnlGrid";
+            this.pnlGrid.Size = new System.Drawing.Size(900, 433);
+            this.pnlGrid.TabIndex = 2;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView.Size = new System.Drawing.Size(900, 433);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // pnlEdit
+            // 
+            this.pnlEdit.Controls.Add(this.pnlEditContent);
+            this.pnlEdit.Controls.Add(this.pnlEditButtons);
+            this.pnlEdit.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlEdit.Location = new System.Drawing.Point(0, 67);
+            this.pnlEdit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlEdit.Name = "pnlEdit";
+            this.pnlEdit.Size = new System.Drawing.Size(900, 433);
+            this.pnlEdit.TabIndex = 3;
+            this.pnlEdit.Visible = false;
+            // 
+            // pnlEditContent
+            // 
+            this.pnlEditContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlEditContent.Location = new System.Drawing.Point(0, 0);
+            this.pnlEditContent.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlEditContent.Name = "pnlEditContent";
+            this.pnlEditContent.Padding = new System.Windows.Forms.Padding(18);
+            this.pnlEditContent.Size = new System.Drawing.Size(900, 383);
+            this.pnlEditContent.TabIndex = 2;
+            // 
+            // pnlEditButtons
+            // 
+            this.pnlEditButtons.Controls.Add(this.btnCancel);
+            this.pnlEditButtons.Controls.Add(this.btnSave);
+            this.pnlEditButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlEditButtons.Location = new System.Drawing.Point(0, 383);
+            this.pnlEditButtons.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlEditButtons.Name = "pnlEditButtons";
+            this.pnlEditButtons.Size = new System.Drawing.Size(900, 50);
+            this.pnlEditButtons.TabIndex = 1;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            //this.btnCancel.Image = global::ERP.Properties.Resources.cancel_icon;
+            this.btnCancel.Location = new System.Drawing.Point(799, 8);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(80, 30);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Hủy";
+            this.btnCancel.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            //this.btnSave.Image = global::ERP.Properties.Resources.save_icon;
+            this.btnSave.Location = new System.Drawing.Point(702, 8);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(80, 30);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Lưu";
+            this.btnSave.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // BaseFormControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pnlGrid);
+            this.Controls.Add(this.pnlEdit);
+            this.Controls.Add(this.pnlToolbar);
+            this.Controls.Add(this.pnlHeader);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "BaseFormControl";
+            this.Size = new System.Drawing.Size(900, 500);
+            this.pnlHeader.ResumeLayout(false);
+            this.pnlHeader.PerformLayout();
+            this.pnlToolbar.ResumeLayout(false);
+            this.pnlGrid.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.pnlEdit.ResumeLayout(false);
+            this.pnlEditButtons.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+        #endregion
+
+        private Panel pnlHeader;
+        private Label lblTitle;
+        private Panel pnlToolbar;
+        private Button btnAdd;
+        private Button btnEdit;
+        private Button btnDelete;
+        private Button btnCopy;
+        private Button btnPrint;
+        private Button btnImportExcel;
+        private Button btnExportExcel;
+        private Button btnRefresh;
+        private Panel pnlGrid;
+        private DataGridView dataGridView;
+        private Panel pnlEdit;
+        private Panel pnlEditButtons;
+        private Button btnCancel;
+        private Button btnSave;
+        private Panel pnlEditContent;
+    }
+}
---
+++ Truct/PCWinForm/PCWinForm/UserControl/BaseFormControl.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PCWinForm.UserControl
+{
+    public partial class BaseFormControl : Form
+    {
+        // Events và Properties giữ nguyên như trước
+        public event EventHandler AddNewClicked;
+        public event EventHandler EditClicked;
+        public event EventHandler DeleteClicked;
+        public event EventHandler CopyClicked;
+        public event EventHandler PrintClicked;
+        public event EventHandler ImportExcelClicked;
+        public event EventHandler ExportExcelClicked;
+        public event EventHandler RefreshClicked;
+        public event EventHandler SaveClicked;
+        public event EventHandler CancelClicked;
+
+        [Category("Data")]
+        [Description("Tiêu đề form")]
+        public string FormTitle
+        {
+            get => lblTitle.Text;
+            set => lblTitle.Text = value;
+        }
+
+        [Browsable(false)]
+        public DataGridView DataGridView => dataGridView;
+
+        [Browsable(false)]
+        public Panel EditPanel => pnlEditContent;
+
+        [Browsable(false)]
+        public bool IsEditMode => pnlEdit.Visible;
+
+        public BaseFormControl()
+        {
+            InitializeComponent();
+            SetupPermissions();
+            InitializeDataGridView();
+        }
+
+        private void InitializeDataGridView()
+        {
+            dataGridView.AutoGenerateColumns = false;
+            dataGridView.AllowUserToAddRows = false;
+            dataGridView.AllowUserToDeleteRows = false;
+            dataGridView.ReadOnly = true;
+            dataGridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView.MultiSelect = false;
+        }
+
+        private void SetupPermissions()
+        {
+            // TODO: Load permissions from user role
+        }
+
+        #region Button Click Handlers
+        private void btnAdd_Click(object sender, EventArgs e) => AddNewClicked?.Invoke(this, e);
+        private void btnEdit_Click(object sender, EventArgs e) => EditClicked?.Invoke(this, e);
+        private void btnDelete_Click(object sender, EventArgs e) => DeleteClicked?.Invoke(this, e);
+        private void btnCopy_Click(object sender, EventArgs e) => CopyClicked?.Invoke(this, e);
+        private void btnPrint_Click(object sender, EventArgs e) => PrintClicked?.Invoke(this, e);
+        private void btnImportExcel_Click(object sender, EventArgs e) => ImportExcelClicked?.Invoke(this, e);
+        private void btnExportExcel_Click(object sender, EventArgs e) => ExportExcelClicked?.Invoke(this, e);
+        private void btnRefresh_Click(object sender, EventArgs e) => RefreshClicked?.Invoke(this, e);
+        private void btnSave_Click(object sender, EventArgs e) => SaveClicked?.Invoke(this, e);
+        private void btnCancel_Click(object sender, EventArgs e) => CancelClicked?.Invoke(this, e);
+        #endregion
+
+        #region Public Methods
+        public void SetButtonPermissions(bool canAdd, bool canEdit, bool canDelete,
+                                       bool canCopy = true, bool canPrint = true,
+                                       bool canExport = true, bool canImport = true)
+        {
+            btnAdd.Enabled = canAdd;
+            btnEdit.Enabled = canEdit;
+            btnDelete.Enabled = canDelete;
+            btnCopy.Enabled = canCopy;
+            btnPrint.Enabled = canPrint;
+            btnExportExcel.Enabled = canExport;
+            btnImportExcel.Enabled = canImport;
+        }
+
+        public void SetEditMode(bool isEditMode)
+        {
+            pnlGrid.Visible = !isEditMode;
+            pnlEdit.Visible = isEditMode;
+
+            if (isEditMode)
+            {
+                btnSave.Enabled = true;
+                btnCancel.Enabled = true;
+            }
+        }
+
+        public void ShowMessage(string message, string title = "Thông báo",
+                              MessageBoxIcon icon = MessageBoxIcon.Information)
+        {
+            MessageBox.Show(message, title, MessageBoxButtons.OK, icon);
+        }
+
+        public DialogResult ShowConfirm(string message, string title = "Xác nhận")
+        {
+            return MessageBox.Show(message, title,
+                                MessageBoxButtons.YesNo,
+                                MessageBoxIcon.Question);
+        }
+
+        public void AddEditControl(Control control)
+        {
+            pnlEditContent.Controls.Add(control);
+            control.Dock = DockStyle.Fill;
+        }
+
+        public void ClearEditControls()
+        {
+            pnlEditContent.Controls.Clear();
+        }
+        #endregion
+#endregion
+    }
+}
