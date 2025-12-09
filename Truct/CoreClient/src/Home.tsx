b1d6e117ca409821a35e5cc0fb3e6a6960c80fd8
latung197 <levantung0000@gmail.com>
2025-12-09 23:11:17 +0700

    ok

---
+++ Truct/CoreClient/src/Home.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+function Home() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+       </div>
+    </>
+  )
+}
+
+export default Home
